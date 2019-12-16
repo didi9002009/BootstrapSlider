2905392d225299b6d01436f65e3b03256b80eb1a
didi9002009 <53850889+didi9002009@users.noreply.github.com>
2019-12-16 23:15:11 +1100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let slideIndex = 0;
 const showSlides = () => {
   // console.log("fn is running");
   const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
   // console.log(slides);
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -12,8 +13,11 @@ const showSlides = () => {
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
-
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 3000);
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 1000);
 };
 showSlides();
