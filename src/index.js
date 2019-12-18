2a82430f98d343ad841df32ab93daeebc5926ef2
didi9002009 <53850889+didi9002009@users.noreply.github.com>
2019-12-18 17:30:40 +1100

    js & css
    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // console.log("text");
-let slideIndex = 0;
+let counter = 0;
 const showSlides = () => {
   // console.log("fn is running");
   const slides = document.getElementsByClassName("mySlides");
@@ -8,16 +8,16 @@ const showSlides = () => {
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
+  counter++;
 
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
+  if (counter > slides.length) {
+    counter = 1;
   }
   for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[counter - 1].style.display = "block";
+  dots[counter - 1].className += " active";
   setTimeout(showSlides, 1000);
 };
 showSlides();
